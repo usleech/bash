eca60cd3834f2a64e05f930bf4bdfb5587774333
usleech <71797515+usleech@users.noreply.github.com>
2022-05-27 02:09:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-sleep-with-countdown() {
-  secs=$1
-  while [ $secs -gt 0 ]; do
-    printf "\rsleep: $secs\033[0K"
-    sleep 1
-    : $((secs--))
-  done
-  printf "\n"
-}
-
-ps -eaf
-
 curl -s ipinfo.io
 sleep 5
 gunicorn server:app --bind 0.0.0.0:$PORT &
